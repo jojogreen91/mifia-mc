bb2b5521a1fcb8112b108a7bda55bff5813fb6a1
jojogreen91 <iawbg13@gmail.com>
2022-01-02 02:35:03 +0900

    프로젝트 생성

---
+++ src/main/java/jojogreenproject/mafiamc/MafiaMcApplication.java
@@ -0,0 +1,13 @@
+package jojogreenproject.mafiamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jojogreenproject/mafiamc/MafiaMcApplicationTests.java
@@ -0,0 +1,13 @@
+package jojogreenproject.mafiamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
